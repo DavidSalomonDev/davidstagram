3d36931cfbe1d404d8fe8c3313a8f575d909fcd5
David ChromeOS <me@davidsalomon.dev>
2021-12-15 08:43:42 -0600

    🗃️ add firebase env variables

--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -8,13 +8,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-	appId: process.env.REACT_APP_FIREBASE_APPID,
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+	apiKey: process.env.NEXT_PUBLIC_APIKEY,
+	authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+	projectId: process.env.NEXT_PUBLIC_PROJECTID,
+	storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+	appId: process.env.NEXT_PUBLIC_APPID,
+	measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 
 // Initialize Firebase
