a8ce87ac80d30419f5f429bbc1074dc6c902fe96
David ChromeOS <me@davidsalomon.dev>
2021-12-15 09:01:34 -0600

    ✨ add firestore

--- firebase/firebase.config.ts
+++ firebase/firebase.config.ts
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,4 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 // const analytics = getAnalytics(app);
